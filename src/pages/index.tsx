9954c3529262ded4d347f7926068bf1596a6178d
marvinrms <ramos.mcontact@gmail.com>
2024-07-12 21:32:32 +0200

    up structure and add home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Welcome to the Home Page
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
